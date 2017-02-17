1b85170784bc4e561f25a5c138e8fe1eff768a52
Tim Kretschmer <tim@krtschmr.de>
2017-02-17 13:32:07 +0700

    auto_flash

--- lib/flash_it.rb
+++ lib/flash_it.rb
@@ -1,11 +1,43 @@
-require "flash_it/version"
-require 'active_support/concern'
+    require "flash_it/version"
+    require 'active_support/concern'
 
-module FlashIt
+    module FlashIt
 
-  extend ActiveSupport::Concern
+      extend ActiveSupport::Concern
 
-  included do
+      module ClassMethods
+        def auto_flash options={}
+          before_render :generate_flash_message, only: [:create, :update, :destroy]
+        end
+      end
+
+      included do
+
+        def generate_flash_message
+          var = instance_variable_get "@#{controller_name.singularize}"
+          if var
+            type = get_flash_type(var)
+
+            #looking in flash_messages.user.something.action.key
+            flash[type] = I18n.t("flash_messages.defaults.#{action_name}.#{type}", obj: var.model_name.human )
+          end
+        end
+
+        def get_flash_type(var)
+          if action_name == "create"
+            return var.persisted? ? :success : :error
+          end
+
+          if action_name == "update"
+            return var.valid? ? :success : :error
+          end
+
+          if action_name == "destroy"
+            return var.persisted? ? :error : :success
+          end
+        end
+
+
 
     # setting flash messages, based on controller/action/key
     def flash_it(key = :success, style=nil)
